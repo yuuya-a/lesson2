2cc634e3a89c9ee3f84139316dd5a4284ac74640
yuuya_A <yuuya001026@icloud.com>
2020-04-26 17:10:45 +0900

    lesson2完了

---
+++ main.js
@@ -0,0 +1,23 @@
+// const nengou = [
+//     '明治',
+//     '大正',
+//     '昭和',
+//     '平成',
+// ]
+
+
+// nengou.push('令和')
+
+// nengou.splice(1, 2)
+
+// nengou[2] = '照和'
+
+
+// const point = 50
+
+
+if (point > 80) {
+    alert('合格')
+}else {
+    alert('不合格です')
+} 
